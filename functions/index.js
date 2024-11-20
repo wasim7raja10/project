148fe3d1cff2208dcce40f45fdfd64e91a3c2a7a
Wasim Raja <wasim7raja10@gmail.com>
2024-11-20 21:58:11 +0530

    cleaned comment

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
 import { onRequest } from "firebase-functions/v2/https";
 import logger from "firebase-functions/logger";
 import { GoogleGenerativeAI } from "@google/generative-ai";
@@ -19,15 +7,6 @@ import * as os from 'os';
 import * as path from 'path';
 import * as fs from 'fs';
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-// Initialize Gemini API
 const genAI = new GoogleGenerativeAI('AIzaSyCEucriHadXPuYjPkrzzzVjfE-Mt5Ggh5E');
 const fileManager = new GoogleAIFileManager('AIzaSyCEucriHadXPuYjPkrzzzVjfE-Mt5Ggh5E');
 
